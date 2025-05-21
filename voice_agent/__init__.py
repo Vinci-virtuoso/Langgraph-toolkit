2fb1bd59255cdca9223aab7da0bce8e262dfba7b
Vinci <5eun3isiyktv@gmail.com>
2025-05-21 18:24:46 +0100

    deleted voice agent folder

--- voice_agent/__init__.py
+++
@@ -1,9 +0,0 @@
-from .agent import setup_voice_agent
-from .pipeline import entrypoint
-from .livekit_types import TypedLivekit
-
-__all__ = [
-    "setup_voice_agent",
-    "entrypoint",
-    "TypedLivekit",
-]
